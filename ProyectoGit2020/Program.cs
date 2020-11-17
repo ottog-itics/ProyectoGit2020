609409d51b31ac79681c149c5766925353be9a8f
Otto Gramajo <oagramajo@gyt.com.gt>
2020-11-16 19:19:10 -0600

    Inicio del proyecto de git 2020.

---
+++ ProyectoGit2020/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProyectoGit2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio del proyecto!");
+            Console.WriteLine("Proyecto para la administración de empleados.");
+            Console.WriteLine("-------------------- Menú -------------------");
+            Console.WriteLine("\t 1) Agregar empleado.");
+            Console.WriteLine("\t 2) Listado de empleados.");
+            Console.WriteLine("\t 3) Salir");            
+        }
+    }
+}
