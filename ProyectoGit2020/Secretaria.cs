ce604093bf742d9b523bf17d3f9ef08833ddde60
ottog-itics <ottog@itics.edu.gt>
2020-12-08 19:53:11 -0600

    Se agrego clase Secretaria.

---
+++ ProyectoGit2020/Secretaria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoGit2020
+{
+    class Secretaria : Empleado
+    {
+        public Secretaria()
+        {
+
+        }
+
+        public Secretaria(string tipoSecretaria, string area)
+        {
+            TipoSecretaria = tipoSecretaria;
+            Area = area;
+        }
+
+        public String TipoSecretaria { get; set; }
+        public String Area { get; set; }
+        public override double CalcularSalario()
+        {
+            return base.Salario * 1.05;
+        }
+    }
+}
